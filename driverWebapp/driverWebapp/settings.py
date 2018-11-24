0b56bc1debc2093df5528f2b6ad7c51f5ab000a0
mmamica <maria.anna.mamica@gmail.com>
2018-11-24 20:33:59 +0100

    commit of a django structure we would work with

--- driverWebapp/driverWebapp/settings.py
+++ driverWebapp/driverWebapp/settings.py
@@ -14,7 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -37,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'data',
+    'route',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'driverWebapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,3 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+TEMPLATE_DIRS = (
+                    os.path.join(os.path.dirname(__file__), 'templates'),
+)
+
--- driverWebapp/route/views.py
+++ driverWebapp/route/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    my_dict = {'insert_me':"Hello I am data hehe"}
+    my_dict = {'insert_me':"Hello I am Marynia hehe"}
     return render(request, 'templates/index.html', context=my_dict)
