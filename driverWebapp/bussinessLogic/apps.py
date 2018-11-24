febdcb46f35641efceddf682563270709818cf0f
Much4cho <Muchacho.ps@gmail.com>
2018-11-24 20:00:36 +0100

    add bussiness logic

---
+++ driverWebapp/bussinessLogic/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BussinesslogicConfig(AppConfig):
+    name = 'bussinessLogic'
--- driverWebapp/data/models.py
+++ driverWebapp/data/models.py
@@ -3,20 +3,27 @@ from django.contrib.auth.models import User
 
 
 class Node(models.Model):
+
     X = models.DecimalField(max_digits=9, decimal_places=3)
     Y = models.DecimalField(max_digits=9, decimal_places=3)
 
     def __str__(self):
-        return "{0} vs {1}".format(
+        return "({0}, {1})".format(
             self.X, self.Y
         )
-    
+
 
 
 class Edge(models.Model):
+
     StartingNode = models.ForeignKey(Node, related_name="StartingNode", on_delete=models.CASCADE)
     EndingNode = models.ForeignKey(Node, related_name="EndingNode", on_delete=models.CASCADE)
     IsClosed = models.BooleanField(default=False)
-    Height = models.DecimalField(max_digits=9, decimal_places=2)
-    Width = models.DecimalField(max_digits=9, decimal_places=2)
-    Weight = models.DecimalField(max_digits=9, decimal_places=2)
+    Height = models.DecimalField(max_digits=9, decimal_places=2, default=0)
+    Width = models.DecimalField(max_digits=9, decimal_places=2, default=0)
+    Weight = models.DecimalField(max_digits=9, decimal_places=2, default=0)
+
+    def __str__(self):
+        return "{0} -> {1}".format(
+            self.StartingNode, self.EndingNode
+        )
