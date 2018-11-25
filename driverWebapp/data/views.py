c9a60df2a29f30e502d4aac06230c397dddff34c
mmamica <maria.anna.mamica@gmail.com>
2018-11-25 04:35:01 +0100

    Merge branch 'master' of https://github.com/Much4cho/WelcomeToTheDjango
    # Conflicts:
    #	driverWebapp/data/views.py
    #	driverWebapp/driverWebapp/settings.py

--- driverWebapp/data/views.py
+++ driverWebapp/data/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 from django.views.decorators.csrf import csrf_exempt
+import json
+import re
+from random import randint
 
 
 
@@ -11,11 +14,22 @@ def index(request):
 
 @csrf_exempt
 def send(request):
-    start = request.POST.get('start')   # end = request.POST.get('end',None)
-    print(request.POST)
-    print("KUPA")
-    print(start)
-    #print(start)
-    #s= Node.objects.get(pk=start)
-    #e= Node.objects.get(pk=end)
+    body_unicode = request.body.decode('utf-8')
+
+    pattern = r'start=(\d+)&end=(\d+)'
+    r = re.compile(pattern)
+    m = r.match(body_unicode)
+    start=m.group(1)
+    end=m.group(2)
+    s= Node.objects.get(pk=start)
+    e= Node.objects.get(pk=end)
+    print(s)
+    print(e)
+    width=[4,5,6,2,3]
+    height=[1,3,4,5,2]
+    available=[true,false]
+    weight=[11,10,9,8,15]
+
     return HttpResponse("lol")
+
+
