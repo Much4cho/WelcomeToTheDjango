c53abd619a813254fcb352c64a3b427e6155c108
Much4cho <Muchacho.ps@gmail.com>
2018-11-25 02:58:48 +0100

    add edges

--- driverWebapp/data/database.py
+++ driverWebapp/data/database.py
@@ -5,14 +5,25 @@
 import csv
 import os
 import io
-from .models import Node
+from .models import *
 
 def load_database():
-    file= "/home/maria/PKN/WelcomeToTheDjango/driverWebapp/data/coordinates.txt"
+    file= "C:\Projects\Hackaton\Welcome to the Django\WelcomeToTheDjango\driverWebapp\data\coordinates.txt"
+
     with open(file) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             _, created = Node.objects.update_or_create(
                 X=row[0],
                 Y=row[1],
+            )
+
+def load_edges():
+    file= "C:\Projects \Hackaton\Welcome to the Django\WelcomeToTheDjango\driverWebapp\data\edges.txt"
+    with open(file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            _, created = Edge.objects.update_or_create(
+                StartingNode_id=row[0],
+                EndingNode_id=row[1],
             )
