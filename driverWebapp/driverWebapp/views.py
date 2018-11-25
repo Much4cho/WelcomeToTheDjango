7edd9d5a3742e41813592fb8f990efb3f2f2c015
agnieszka <agapekala1@wp.pl>
2018-11-25 01:25:10 +0100

    sending nodes

--- driverWebapp/driverWebapp/views.py
+++ driverWebapp/driverWebapp/views.py
@@ -1,6 +1,18 @@
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
+from django.shortcuts import render
+from data.models import Node
+from  django.template import RequestContext
 
-class MainPage(TemplateView):
-    template_name = 'index.html'
+def index(request):
+    nodes=Node.objects.all()
+    my_dict = {'nodes': nodes}
+    return render(request, 'index.html', context=my_dict)
+
+
+def test(request):
+    my_dict = {'nodes': "nodes"}
+    return render(request, 'test.html', context=my_dict)
+
+
